ce92761372771eaf106da7db1d83a5fdb64f3fe7
Jonas Bird <jonas.bird.m@gmail.com>
2022-12-20 00:49:15 -0500

    feat: Add support for Spanish greetings

--- hello/hello.go
+++ hello/hello.go
@@ -2,15 +2,21 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const greetingEnglish = "Hello, "
+const greetingSpanish = "Hola, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if language == spanish {
+		return greetingSpanish + name
 	}
 	return greetingEnglish + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,17 +4,22 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Jonas")
+		got := Hello("Jonas", "")
 		want := "Hello, Jonas"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 	})
 
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {
